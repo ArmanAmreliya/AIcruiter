9c47afd674a603731f899884c9232d5d7e80c8b5
jimmyhomie404-prog <armanamreliya33@gmail.com>
2026-07-05 00:34:27 +0530

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -10,7 +10,10 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['127.0.0.1'],
+  allowedDevOrigins: ["127.0.0.1"],
+  turbopack: {
+    root: path.resolve(__dirname, "../.."),
+  },
   env: {
     NEXT_SUPABASE_URL: process.env.NEXT_SUPABASE_URL,
     NEXT_SUPABASE_ANON_KEY: process.env.NEXT_SUPABASE_ANON_KEY,
